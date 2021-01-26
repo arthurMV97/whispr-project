e56c9301856356a0ac0ce5d38ca9008d1f0c885d
Arthur <morinarthurpro@gmail.com>
2021-01-26 13:04:06 +0100

    sql for posts with total likes and comment

--- api/routes/poste.js
+++ api/routes/poste.js
@@ -13,9 +13,10 @@ router.get("/post", (req, res) => {
 router.get("/post/:userID", (req, res) => {
     const user_id = req.params.userID 
 
-    connection.query(`SELECT * FROM post INNER JOIN abonnement ON post.user_id = abonnement.compte_abonnement_id WHERE abonnement.user_id = ${user_id} AND post.reponse_id IS NULL`, (err, result) => {
+    connection.query(`SELECT post.id, post.content, post.date, post.user_id, user.nom, user.prenom, user.image, (SELECT COUNT(*) FROM favoris WHERE favoris.post_id = post.id) AS TotalFavoris, (SELECT COUNT(*) FROM post WHERE post.reponse_id = post.id) AS TotalComment FROM post INNER JOIN user ON post.user_id = user.id INNER JOIN abonnement ON post.user_id = abonnement.compte_abonnement_id WHERE abonnement.user_id = ${user_id} AND post.reponse_id IS NULL`, (err, result) => {
         if (err) throw err
-        res.status(200).send(result)
+        let postes = result
+        res.status(200).send(postes)
     })
 })
 
--- whispr-app/src/components/Dashboard.jsx
+++ whispr-app/src/components/Dashboard.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 import { io } from 'socket.io-client';
 let socket
 
@@ -7,7 +9,8 @@ class Dashboard extends Component {
     constructor() {
         super();
         this.state = {
-            data: ''
+            posteToSend: '',
+            postes: []
         }
     }
     handleChange = event => {
@@ -17,27 +20,47 @@ class Dashboard extends Component {
 
     handleSubmit = event => {
         event.preventDefault();
-        const dataToEmit = this.state.data
+        const dataToEmit = this.state.posteToSend
         socket.emit('posteData', dataToEmit)
     }
 
     componentDidMount() {
+        axios.get(`http://localhost:8080/post/${this.props.userId}`, )
+        .then(res => {
+            this.setState({postes: res.data})
+            console.log(this.state.postes)
+        })
         socket = io('localhost:8080')
         socket.on('message', (data) => {
             console.log(data)
-            console.log('hi')
         })
     }
     render() {
         return (
             <div>
                 <form>
-                        <textarea placeholder="Racontez-nous quelque chose..." onChange={this.handleChange}></textarea>
+                        <textarea placeholder="Racontez-nous quelque chose..." name="posteToSend" onChange={this.handleChange}></textarea>
                         <button className="full-btn">Publier</button>
                     </form>
+                <div className="feed">
+                    {
+                        this.state.postes.map((element, index) => {
+                           return (
+                           <div className="poste" key={index}>Element numéro {index}</div>
+                           )
+                        })
+                    }
+                    
+                </div>
             </div>
         );
     }
 }
 
-export default Dashboard;
+function mapStateToProps(state) { 
+    return {
+      userId: state.userStore.id
+    };
+  }
+
+export default connect(mapStateToProps)(Dashboard);
